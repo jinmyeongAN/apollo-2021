709e655e0de2ce28bc3e9caf3d275d83d4d03d3d
jinmyeongAN <jinmyeong.an@gmail.com>
2021-08-10 19:51:53 +0900

    #2 graphql in react

--- src/apollo.js
+++ src/apollo.js
@@ -1,5 +1,8 @@
-import ApolloClient from "apollo-client";
+import {ApolloClient, InMemoryCache} from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 
-})
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache(),
+  });
+
+  export default client;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,50 @@
-import React from "react"
+import React, { Title } from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+@@ -36,26 +37,29 @@
+  margin-bottom: 20px;
+`;
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+export default () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  return (
+    <Container>
+
+      {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map(m => <Movie key={m.id} id={m.id} />)}
+    </Container>
+  );
+};
